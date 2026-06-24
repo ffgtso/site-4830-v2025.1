5c4e44168d22b2366c3e749a1d5f19d54b70f81a
wusel <wusel+src@uu.org>
2026-06-24 23:49:24 +0200

    fuck it

--- image-customization.lua
+++ image-customization.lua
@@ -68,7 +68,7 @@ tiny_like = device({
     'tp-link-wbs210-v2',
     'tp-link-wbs510-v1',
     'ubiquiti-nanostation-loco-m-xw',
-    'ubiquiti-unifi-ap',
+    'ubiquiti-unifi',
     'tp-link-archer-c60-v1',
 })
 
